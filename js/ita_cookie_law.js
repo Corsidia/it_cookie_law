79db9198c3159c4148a9cce91da8e93fc0e79345
nemboweb <team@nemboweb.it>
2015-10-16 11:52:37 +0000

    added support for head scripts

--- js/ita_cookie_law.js
+++ js/ita_cookie_law.js
@@ -1,5 +1,27 @@
-// cambia 'cookie_policy_accepted' col nome che vuoi, sarà il nome del cookie impostato
-var acceptedCookieName = 'cookie_policy_accepted'
+var acceptedCookieName = 'cookie_policy_accepted' // puoi cambiarlo col nome che vuoi, sarà il nome del cookie impostato
+var cookiePolicyURL = "http://example.com/cookie-policy" // QUESTO DEVE ESSERE IMPOSTATO CORRETTAMENTE!
+var infoBannerId = "cookie_info_breve" // Deve essere univoco all'interno della pagina
+var acceptButtonId = "cookie_accept_button" // Deve essere univoco all'interno della pagina
+
+var infoText = "Questo sito utilizza i cookie, anche di terze parti: cliccando su 'Chiudi', proseguendo nella navigazione, effettuando lo scroll della pagina o altro tipo di interazione col sito, acconsenti all'utilizzo dei cookie. Per maggiori informazioni o per negare il consenso a tutti o ad alcuni cookie, consulta l'informativa."
+var infoLinkText = "Leggi informativa"
+var acceptButtonText = "Chiudi"
+var divEsternoCSS = "position: fixed; width: 100%; background-color: rgba(0, 0, 0, 0.7); margin: 0px; left: 0px; top: 0px; padding: 10px 0px; z-index: 1000;";
+var divInternoCSS = "position: relative; width: 80%; margin: 0px auto;";
+var infoTextCSS = "display: block; width: 800px; line-height: 20px; color: rgb(255, 255, 255); font-family: verdana,arial,tahoma,sans-serif; font-size: 12px;";
+var infoLinkCSS = "position: absolute; top: 30px; right: 0px; display: block; width: 140px; line-height: 30px; color: rgb(255, 255, 255); text-align: center; font-size: 12px; text-decoration: underline;";
+var acceptButtonCSS = "position: absolute; top: 0px; right: 0px; display: block; width: 140px; line-height: 30px; color: rgb(255, 153, 0); text-align: center; font-size: 14px; font-weight: bold; text-decoration: none;";
+
+// Costruttore del banner informativo
+var infoBanner = "<div id='"+infoBannerId+"' style='"+divEsternoCSS+"'>" +
+                   "<div style='"+divInternoCSS+"'>" +
+                     "<span style='"+infoTextCSS+"'>" +
+                       infoText +
+                     "</span>" +
+                     "<a href='"+cookiePolicyURL+"' target='_blank' style='"+infoLinkCSS+"'>"+infoLinkText+"</a>" +
+                     "<a href='#' id='"+acceptButtonId+"' style='"+acceptButtonCSS+"'>"+acceptButtonText+"</a>"
+                   "</div>" +
+                 "</div>";
 
 $(document).ready(function() {
   if (getCookie(acceptedCookieName) === 'true') { 
@@ -10,10 +32,11 @@ $(document).ready(function() {
 });
 
 function optInHandler(){
-  $('#cookie_info_breve').show();
+  $('body').append(infoBanner);
   setTimeout(readUserInput, 2000) // aspetta due secondi per dar tempo all'utente di notare il banner
 }
 function readUserInput(){
+  // Accettazione mediante scroll
   var scrolled = false;
   window.onscroll = function(e){
     if (scrolled == false){
@@ -21,17 +44,35 @@ function readUserInput(){
       cookieOptIn();
     }
   }
+  // Accettazione con click su acceptButton
+  $('#'+acceptButtonId).click(function() {
+    cookieOptIn();
+  });
 } 
 function cookieOptIn(){
   setCookie(acceptedCookieName, 'true', 1000);
-  $('#cookie_info_breve').hide();
+  $('#'+infoBannerId).hide();
   optedIn();
 }
+
+function reloadJs(src) {
+  src = $('script[src$="' + src + '"]').attr("src");
+  $('script[src$="' + src + '"]').remove();
+  $('<script/>').attr('src', src).appendTo('body');
+}
+
 function optedIn(){
-  $("[type='text/plain']").each(function(){
-    $(this).attr('type', 'text/javascript'); //cambia il type dello script per renderlo eseguibile
-    $.globalEval($(this).html()); //esegui lo script
-  }); 
+// head scripts with type=text/plain
+$("head script[type='text/plain']").each(function(){
+  reloadJs($(this).attr('src'));
+});
+
+// body script with type=text/plain
+$("body script[type='text/plain']").each(function(){
+  $(this).attr('type', 'text/javascript'); //cambia il type dello script per renderlo eseguibile
+  $.globalEval($(this).html()); //esegui lo script
+});
+
 }
 
 // Cookie get, set and delete functions
